eb958cd9f32923bc7125f2d9f509f2ddec6d3dc3
mohammadsaati <mohammadtbz76@gmail.com>
2023-05-29 00:40:01 +0330

    home page completing...

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Content extends Model
 {
     use HasFactory;
+
+    /***************************************************
+     ***************** #Relations **********************
+     ****************** #Start *************************/
+    public function product()
+    {
+        return $this->belongsTo(Product::class , "content_id");
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class , "category_id");
+    }
+    /***************************************************
+     ***************** #Relations **********************
+     ****************** #End *************************/
+
 }
